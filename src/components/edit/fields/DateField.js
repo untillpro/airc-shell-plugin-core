532e95825716b9285a4e5d8a819b4acae9faadd5
Yohanson555 <yohanson555@gmail.com>
2020-09-15 15:50:51 +0300

    now wors with timestamp

--- src/components/edit/fields/DateField.js
+++ src/components/edit/fields/DateField.js
@@ -4,20 +4,21 @@
 
 import React, { Component } from 'react';
 import moment from 'moment';
-import { DatePicker } from '../../../base/components'
+import { DatePicker } from '../../../base/components';
+
+import log from '../../../classes/Log';
+
+const DEFAULT_DATE_FORMAT = "DD/MM/YYYY";
 
 class DateField extends Component {
 
     handleChange(date, dateString) {
-        const { onChange, field } = this.props;
-        let outFormat = 'YYYY-MM-DD';
+        log("DateField changed: ", date, dateString, date.valueOf());
 
-        if (field && field.outFormat) {
-            outFormat = String(field.outFormat);
-        }
+        const { onChange, field } = this.props;
 
         if (onChange && typeof onChange === 'function' ) {
-            onChange(date ? date.format(outFormat) : null);
+            onChange(date ? date.valueOf() : null);
         }
     }
 
@@ -28,20 +29,22 @@ class DateField extends Component {
     getFormat() {
         const { field } = this.props;
 
-        let format = 'YYYY-MM-DD';
+        let format = DEFAULT_DATE_FORMAT;
 
-        if (field && field.format) format = field.format;
+        if (field && field.format && typeof field.format === 'string') {
+            format = field.format;
+        }
 
         return format;
     }
 
     getValue() {
         const { value } = this.props;
-        const format = this.getFormat();
+        if (!value) return moment();
 
-        if (!value) return null;
+        log('Date-time field value: ', value);
 
-        return moment(value, format);
+        return moment(value);
     }
     
     render() {
@@ -55,9 +58,10 @@ class DateField extends Component {
             <DatePicker 
                 {...props}
                 disabled={disabled}
-                value={this.getValue()}
+                format={this.getFormat()}
+                //value={this.getValue()}
                 //defaultValue={moment(new Date().valueOf(), this.getFormat())}
-                defaultValue={null}
+                defaultValue={this.getValue()}
                 onChange={this.handleChange.bind(this)} 
             />
         );
