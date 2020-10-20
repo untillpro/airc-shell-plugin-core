d571ebc875aa1fa1654ed337910f995979fe9b59
Evgeniy Saigachenko <yohanson555@gmail.com>
2020-10-20 12:21:56 +0300

    new complex type

--- src/classes/contributions/Types.js
+++ src/classes/contributions/Types.js
@@ -59,6 +59,7 @@ export const C_REPORT_FIELDS = "fields";
 export const C_REPORT_COLUMNS = "columns";
 export const C_REPORT_TABLE_PROPS = "table_props";
 export const C_REPORT_EVENT_TYPE = "event_type";
+export const C_REPORT_COMPLEX = "is_complex";
 
 // ### EMBEDED MANAGERS
 
