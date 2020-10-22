03d451f94dfa2b4648ab3d04bfb1bc25f6a3dc1b
Evgeniy Saigachenko <yohanson555@gmail.com>
2020-10-22 13:00:22 +0300

    PAYMENT_KIND_INVERT const added;

--- src/const/PaymentKind.js
+++ src/const/PaymentKind.js
@@ -20,3 +20,22 @@ export const PAYMENT_KIND = {
     14: "Zapper",
     15: "Coupon",
 };
+
+export const PAYMENT_KIND_INVERT = {
+    "Cash": 0,
+    "Card": 1,
+    "Account": 2,
+    "Room": 3,
+    "Cheque": 4,
+    "Hash": 5,
+    "Service Charge": 6,
+    "Discount": 7,
+    "Smart Card": 8,
+    "Sachdro": 9,
+    "Newyse On-account": 10,
+    "Gift Card": 11,
+    "Prepaid Card Beverage Control": 12,
+    "Voucher": 13,
+    "Zapper": 14,
+    "Coupon": 15,
+};
