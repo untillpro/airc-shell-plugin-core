c20c48c1abfb8597cddd52525cef5ee3ed6ee534
Yohanson555 <yohanson555@gmail.com>
2021-02-18 20:40:08 +0300

    temp solution with blob

--- src/components/common/plan/TableAreaImageSelect.js
+++ src/components/common/plan/TableAreaImageSelect.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Empty, Upload, Button, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 
+const uploadFileAction = "https://badrequest.ru/tests/uploader/write.php"; //TODO: mock
 
 class TableAreaImageSelect extends PureComponent {
     constructor(props) {
@@ -13,6 +14,30 @@ class TableAreaImageSelect extends PureComponent {
         this.customRequest = this.customRequest.bind(this);
     }
 
+    _getError(option, xhr) {
+        const msg = `cannot ${option.method} ${option.action} ${xhr.status}'`;
+        const err = new Error(msg);
+
+        err.status = xhr.status;
+        err.method = option.method;
+        err.url = option.action;
+
+        return err;
+    }
+
+    _getBody(xhr) {
+        const text = xhr.responseText || xhr.response;
+        if (!text) {
+            return text;
+        }
+
+        try {
+            return JSON.parse(text);
+        } catch (e) {
+            return text;
+        }
+    }
+    
     handleChange(info) {
         const { setImage } = this.props;
 
@@ -31,11 +56,87 @@ class TableAreaImageSelect extends PureComponent {
         }
     }
 
-    customRequest(data) {
-        const { context } = this.props;
-        const { api } = context;
+    customRequest(option) {
+        const xhr = new XMLHttpRequest();
 
-        api.blob(data);
+        if (option.onProgress && xhr.upload) {
+            xhr.upload.onprogress = function progress(e) {
+                if (e.total > 0) {
+                    e.percent = (e.loaded / e.total) * 100;
+                }
+                option.onProgress(e);
+            };
+        }
+
+        // eslint-disable-next-line no-undef
+        const formData = new FormData();
+
+        if (option.data) {
+            Object.keys(option.data).forEach(key => {
+                const value = option.data[key];
+                // support key-value array data
+                if (Array.isArray(value)) {
+                    value.forEach(item => {
+                        // { list: [ 11, 22 ] }
+                        // formData.append('list[]', 11);
+                        formData.append(`${key}[]`, item);
+                    });
+                    return;
+                }
+
+                formData.append(key, option.data[key]);
+            });
+        }
+
+        // eslint-disable-next-line no-undef
+        if (option.file instanceof Blob) {
+            formData.append(option.filename, option.file, option.file.name);
+        } else {
+            formData.append(option.filename, option.file);
+        }
+
+        xhr.onerror = function error(e) {
+            option.onError(e);
+        };
+
+        xhr.onload = () => {
+            // allow success when 2xx status
+            // see https://github.com/react-component/upload/issues/34
+            if (xhr.status < 200 || xhr.status >= 300) {
+                return option.onError(this._getError(option, xhr), this._getBody(xhr));
+            }
+
+            return option.onSuccess(this._getBody(xhr), xhr);
+        };
+
+        xhr.open(option.method, uploadFileAction, true);
+
+        // Has to be after `.open()`. See https://github.com/enyo/dropzone/issues/179
+        if (option.withCredentials && 'withCredentials' in xhr) {
+            xhr.withCredentials = true;
+        }
+
+        const headers = option.headers || {};
+
+        // when set headers['X-Requested-With'] = null , can close default XHR header
+        // see https://github.com/react-component/upload/issues/33
+        if (headers['X-Requested-With'] !== null) {
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        }
+
+        Object.keys(headers).forEach(h => {
+            if (headers[h] !== null) {
+                xhr.setRequestHeader(h, headers[h]);
+            }
+        });
+
+        xhr.send(formData);
+
+        return {
+            abort() {
+                xhr.abort();
+            },
+        };
     }
 
     render() {
